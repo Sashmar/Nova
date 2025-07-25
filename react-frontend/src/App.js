3f69f5ecec372bbc05669442ac4e5722c23b1dcb
Sashmar <sajinskmr22@gmail.com>
2025-07-26 00:08:06 +0530

    Initial clean commit with Git LFS configured and core project files

---
+++ react-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // Import the global styles
+
+function App() {
+  return (
+    <div className="nova-browser-container">
+      {/* This is where all other browser components will eventually go */}
+      <p>Nova Browser UI will load here.</p>
+    </div>
+  );
+}
+
+export default App;
